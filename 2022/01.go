1c6bd6a85f91ab22b102a6d72a4287ebe0ee811d
ae <david.uhrik@resistant.ai>
2023-11-30 23:35:13 +0100

    2022/01 part2

--- 2022/01.go
+++ 2022/01.go
@@ -4,31 +4,45 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
+// assume arr is sorted and non-empty
+func insertNum(arr []int, num int) []int {
+	if num > arr[0] {
+		arr = append(arr[1:], num)
+		sort.Ints(arr)
+	}
+	return arr
+}
+
+func sum(arr []int) int {
+	s := 0
+	for _, v := range arr {
+		s += v
+	}
+	return s
+}
+
 func main() {
 	file, _ := os.Open("inputs/01")
-
 	scanner := bufio.NewScanner(file)
 
-	maxElf := 0
+	N := 3
+	maxElves := make([]int, N)
 	currElf := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		if line == "" {
-			if currElf > maxElf {
-				maxElf = currElf
-			}
+			maxElves = insertNum(maxElves, currElf)
 			currElf = 0
 		} else {
 			currCal, _ := strconv.Atoi(line)
 			currElf += currCal
 		}
 	}
-	if currElf > maxElf {
-		fmt.Println(currElf)
-	} else {
-		fmt.Println(maxElf)
-	}
+	maxElves = insertNum(maxElves, currElf)
+
+	fmt.Println(sum(maxElves))
 }
