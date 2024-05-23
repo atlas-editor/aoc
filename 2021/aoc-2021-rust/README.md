6f4524360f68a9adfaa575c5f24fe988935273c8
ae <david.uhrik@resistant.ai>
2024-05-23 06:44:34 +0200

    d6 only use better solution

--- 2021/aoc-2021-rust/README.md
+++ 2021/aoc-2021-rust/README.md
@@ -3,18 +3,18 @@ Benchmark on Apple M1 Pro (times exclude input reading and result printing).
 ```
 day       part 1    part 2
 ------------------------------
-day 01    0.155 ms  0.150 ms
-day 02    0.193 ms  0.160 ms
-day 03    0.422 ms  0.233 ms
-day 04    0.671 ms  1.522 ms
-day 05    54.23 ms  41.69 ms
-day 06    5.331 ms  0.117 ms
-day 07    0.261 ms  0.177 ms
-day 08    0.021 ms  1.034 ms
-day 09    0.417 ms  1.973 ms
-day 10    0.122 ms  0.124 ms
-day 11    0.134 ms  0.261 ms
-day 12    3.038 ms  335.5 ms
-day 13    0.239 ms  0.107 ms
-day 14    1.289 ms  -
+day 01    0.225 ms  0.114 ms
+day 02    0.172 ms  0.165 ms
+day 03    0.415 ms  0.252 ms
+day 04    0.758 ms  1.572 ms
+day 05    55.21 ms  41.75 ms
+day 06    0.110 ms  0.065 ms
+day 07    0.208 ms  0.189 ms
+day 08    0.022 ms  0.797 ms
+day 09    0.426 ms  2.289 ms
+day 10    0.134 ms  0.137 ms
+day 11    0.148 ms  0.279 ms
+day 12    3.212 ms  342.4 ms
+day 13    0.246 ms  0.105 ms
+day 14    1.239 ms  -
 ```
--- 2021/aoc-2021-rust/src/days/d06.rs
+++ 2021/aoc-2021-rust/src/days/d06.rs
@@ -4,41 +4,24 @@ use crate::utils::ints;
 
 pub fn p1(raw_input: &str) -> u64 {
     let input = parse_input(raw_input);
-    _p1(&input)
+    p(&input, 80)
 }
 
 pub fn p2(raw_input: &str) -> u64 {
     let input = parse_input(raw_input);
-    _p2(&input)
+    p(&input, 256)
 }
 
 fn parse_input(raw_input: &str) -> Vec<usize> {
     raw_input.lines().next().map(ints).unwrap()
 }
 
-fn _p1(nums: &Vec<usize>) -> u64 {
-    let mut c = nums.to_owned();
-    for _ in 0..80 {
-        let mut ch = 0;
-        for (i, e) in c.clone().iter().enumerate() {
-            if *e == 0 {
-                c[i] = 6;
-                ch += 1;
-            } else {
-                c[i] -= 1;
-            }
-        }
-        c.append(&mut vec![8; ch]);
-    }
-    c.len() as u64
-}
-
-fn _p2(nums: &Vec<usize>) -> u64 {
+fn p(nums: &Vec<usize>, loops: u16) -> u64 {
     let mut state = VecDeque::from([0; 9]);
     for i in nums {
         state[*i] += 1;
     }
-    for _ in 0..256 {
+    for _ in 0..loops {
         state.rotate_left(1);
         state[6] += state[8];
     }
