49e5060a9ed39416b84c1385b4f1cc33d1a3eeba
ae <david.uhrik@resistant.ai>
2024-06-04 20:38:26 +0200

    day14 use dp table

--- 2021/aoc-2021-rust/src/days/d14.rs
+++ 2021/aoc-2021-rust/src/days/d14.rs
@@ -1,57 +1,112 @@
 use itertools::Itertools;
 use std::collections::HashMap;
-use std::fmt::Write;
 
-pub fn p1(raw_input: &str) -> i32 {
+pub fn p1(raw_input: &str) -> usize {
     let (template, rules) = parse_input(raw_input);
-    _p1(template, &rules)
+    _p(template, &rules, 10)
 }
 
-pub fn p2(_raw_input: &str) -> i32 {
-    -1
+pub fn p2(raw_input: &str) -> usize {
+    let (template, rules) = parse_input(raw_input);
+    _p(template, &rules, 40)
 }
 
-fn parse_input(raw_input: &str) -> (String, HashMap<String, String>) {
+fn parse_input(raw_input: &str) -> (Vec<usize>, HashMap<(usize, usize), usize>) {
+    let chars = "BCFHKNOPSV";
     let input = raw_input.split_once("\n\n").unwrap();
-    let template = input.0.to_string();
+    let template = input.0.chars().map(|x| chars.find(x).unwrap()).collect();
     let rules = input
         .1
         .lines()
         .map(|x| x.split_once(" -> ").unwrap())
-        .map(|x| (x.0.to_string(), x.1.to_string()))
+        .map(|x| {
+            (
+                (
+                    chars.find(x.0.chars().next().unwrap()).unwrap(),
+                    chars.find(x.0.chars().nth(1).unwrap()).unwrap(),
+                ),
+                chars.find(x.1).unwrap(),
+            )
+        })
         .collect();
     (template, rules)
 }
 
-fn apply_rules(template: String, rules: &HashMap<String, String>) -> String {
-    template
-        .chars()
-        .tuple_windows::<(char, char)>()
-        .fold(String::new(), |mut acc, x| {
-            let _ = write!(
-                acc,
-                "{}{}",
-                x.0,
-                *rules
-                    .get(format!("{}{}", x.0, x.1).as_str())
-                    .unwrap_or(&String::new())
-            );
-            acc
-        })
-        + &template.chars().last().unwrap().to_string()
+fn add_vectors(vectors: &[&Vec<usize>]) -> Vec<usize> {
+    let mut result = vectors[0].clone();
+    for i in 1..vectors.len() {
+        for j in 0..result.len() {
+            result[j] += vectors[i][j];
+        }
+    }
+    result
 }
 
-fn _p1(mut template: String, rules: &HashMap<String, String>) -> i32 {
-    for _ in 0..10 {
-        template = apply_rules(template, rules);
+fn dp(rules: &HashMap<(usize, usize), usize>, d: usize) -> Vec<Vec<Vec<usize>>> {
+    let zero = vec![0; 10];
+    let mut table = vec![vec![vec![zero.clone(); 10]; 10]; d+1];
+    for i in 1..d+1 {
+        for j0 in 0..10 {
+            for j1 in 0..10 {
+                if !rules.contains_key(&(j0, j1)) {
+                    table[i][j0][j1] = zero.clone();
+                    continue;
+                }
+                let mid = rules.get(&(j0, j1)).unwrap();
+                let c1 = table[i - 1][j0][*mid].clone();
+                let c2 = table[i - 1][*mid][j1].clone();
+                let mut c = zero.clone();
+                c[*mid] += 1;
+                table[i][j0][j1] = add_vectors(&[&c, &c1, &c2]);
+            }
+        }
+    }
+    table[d].clone()
+}
+
+fn _p(template: Vec<usize>, rules: &HashMap<(usize, usize), usize>, d: usize) -> usize {
+    let table = dp(rules, d);
+    let mut result = vec![0; 10];
+    for digit in template.clone() {
+        result[digit] += 1;
+    }
+    for i in 0..template.len() - 1 {
+        let v = table[template[i]][template[i + 1]].clone();
+        result = add_vectors(&[&result, &v]);
+    }
+    let max_ = result.iter().max().unwrap();
+    let min_ = result.iter().filter(|&&x| x != 0).min().unwrap();
+    max_ - min_
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(p1(raw_input()), 1588);
+        assert_eq!(p2(raw_input()), 2188189693529);
     }
 
-    let (&min_, &max_) = template
-        .chars()
-        .counts()
-        .values()
-        .minmax()
-        .into_option()
-        .unwrap();
-    max_ as i32 - min_ as i32
+    fn raw_input<'a>() -> &'a str {
+        "NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C"
+    }
 }
