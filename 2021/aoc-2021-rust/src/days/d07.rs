93f9082a771d29d12be26347ab9b5217208bbfab
ae <david.uhrik@resistant.ai>
2024-06-09 09:42:59 +0200

    more correcter version

--- 2021/aoc-2021-rust/src/days/d07.rs
+++ 2021/aoc-2021-rust/src/days/d07.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::cmp::min;
 
 pub fn p1(raw_input: &str) -> i32 {
     let input = parse_input(raw_input);
@@ -24,15 +25,15 @@ fn find_min<T: Fn(i32) -> i32>(f: T, nums: &[i32]) -> i32 {
     let (a_ref, b_ref) = nums.iter().minmax().into_option().unwrap();
     let (mut a, mut b) = (*a_ref, *b_ref);
     let g = |i: i32| nums.iter().map(|n| f((n - i).abs())).sum::<i32>();
-    while a != b {
+    while a + 1 != b {
         let mid = (a + b) / 2;
-        if g((a + mid) / 2) <= g((mid + b) / 2) {
+        if g(mid - 1) <= g(mid + 1) {
             b = mid;
         } else {
             a = mid;
         }
     }
-    g(a)
+    min(g(a), g(b))
 }
 
 #[cfg(test)]
