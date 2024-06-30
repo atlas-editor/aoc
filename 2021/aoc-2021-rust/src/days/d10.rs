bcf7108bdd163fdf36fb904852fea4db02d4cb2f
ae <david.uhrik@resistant.ai>
2024-06-30 11:45:37 +0200

    more functional

--- 2021/aoc-2021-rust/src/days/d10.rs
+++ 2021/aoc-2021-rust/src/days/d10.rs
@@ -1,6 +1,7 @@
+use itertools::Itertools;
 use std::collections::HashMap;
 
-pub fn p1(raw_input: &str) -> i32 {
+pub fn p1(raw_input: &str) -> u32 {
     let input = parse_input(raw_input);
     _p1(&input)
 }
@@ -14,58 +15,60 @@ fn parse_input(raw_input: &str) -> Vec<&str> {
     raw_input.lines().collect()
 }
 
-fn _p1(lines: &[&str]) -> i32 {
-    let vals = [(')', 3), (']', 57), ('}', 1197), ('>', 25137)]
-        .into_iter()
-        .collect::<HashMap<_, _>>();
-    let mut res = 0;
-    for line in lines {
+enum LineType {
+    Incomplete(Vec<char>),
+    Corrupted(char),
+}
+
+impl LineType {
+    fn from_str(line: &str) -> Self {
         let mut stack = vec![];
         for ch in line.chars() {
             if [')', ']', '}', '>'].contains(&ch) {
                 if let Some(pair) = stack.pop() {
                     let t = format!("{pair}{ch}");
                     if !["()", "[]", "{}", "<>"].contains(&t.as_str()) {
-                        res += vals[&ch];
-                        break;
+                        return LineType::Corrupted(ch);
                     }
                 } else {
-                    res += vals[&ch];
-                    break;
+                    return LineType::Corrupted(ch);
                 }
             } else {
                 stack.push(ch);
             }
         }
+        LineType::Incomplete(stack)
     }
-    res
+}
+
+fn _p1(lines: &[&str]) -> u32 {
+    let vals = [(')', 3), (']', 57), ('}', 1197), ('>', 25137)]
+        .into_iter()
+        .collect::<HashMap<_, _>>();
+    lines
+        .iter()
+        .map(|line| match LineType::from_str(line) {
+            LineType::Incomplete(_) => 0,
+            LineType::Corrupted(ch) => vals[&ch],
+        })
+        .sum()
 }
 
 fn _p2(lines: &[&str]) -> u64 {
     let vals = [('(', 1), ('[', 2), ('{', 3), ('<', 4)]
         .into_iter()
         .collect::<HashMap<_, _>>();
-    let mut res = vec![];
-    'outer: for line in lines {
-        let mut stack = vec![];
-        for ch in line.chars() {
-            if [')', ']', '}', '>'].contains(&ch) {
-                if let Some(pair) = stack.pop() {
-                    let t = format!("{pair}{ch}");
-                    if !["()", "[]", "{}", "<>"].contains(&t.as_str()) {
-                        continue 'outer;
-                    }
-                } else {
-                    continue 'outer;
-                }
-            } else {
-                stack.push(ch);
+    let scores = lines
+        .iter()
+        .filter_map(|line| match LineType::from_str(line) {
+            LineType::Incomplete(stack) => {
+                Some(stack.iter().rev().fold(0, |acc, ch| acc * 5 + vals[ch]))
             }
-        }
-        res.push(stack.iter().rev().fold(0, |acc, ch| acc * 5 + vals[ch]));
-    }
-    res.sort();
-    res[(res.len() - 1) / 2]
+            LineType::Corrupted(_) => None,
+        })
+        .sorted()
+        .collect_vec();
+    scores[(scores.len() - 1) / 2]
 }
 
 #[cfg(test)]
