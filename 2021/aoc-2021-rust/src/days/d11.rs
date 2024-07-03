737a68aaa3ab609dd0af7015df423c3e7c634145
ae <david.uhrik@resistant.ai>
2024-07-03 06:15:55 +0200

    "optimization" 3

--- 2021/aoc-2021-rust/src/days/d11.rs
+++ 2021/aoc-2021-rust/src/days/d11.rs
@@ -1,11 +1,12 @@
 use crate::days::utils::*;
+use std::cmp::max;
 
-pub fn p1(raw_input: &[u8]) -> i32 {
+pub fn p1(raw_input: &[u8]) -> i16 {
     let mut input = parse_input(raw_input);
     p(&mut input, false)
 }
 
-pub fn p2(raw_input: &[u8]) -> i32 {
+pub fn p2(raw_input: &[u8]) -> i16 {
     let mut input = parse_input(raw_input);
     p(&mut input, true)
 }
@@ -20,6 +21,7 @@ fn neighbors(
     r: usize,
     c: usize,
     m: &Matrix<i16>,
+    i: i16,
 ) -> Vec<(usize, usize)> {
     let mut nbrs = vec![];
     for (dr, dc) in [
@@ -39,7 +41,7 @@ fn neighbors(
             && rr < r_size as i8
             && cc >= 0
             && cc < c_size as i8
-            && m[rr as usize][cc as usize] != -1
+            && m[rr as usize][cc as usize] != -i - 9
         {
             nbrs.push((rr as usize, cc as usize))
         }
@@ -48,49 +50,48 @@ fn neighbors(
     nbrs
 }
 
-fn p(energy: &mut Matrix<i16>, p2: bool) -> i32 {
+fn p(energy: &mut Matrix<i16>, p2: bool) -> i16 {
     let (r_size, c_size) = energy.shape;
     let mut flashes = 0;
     for i in 0.. {
-        let mut step_flashes = vec![];
+        let mut step_flashes = 0;
         if i == 100 && !p2 {
             return flashes;
         }
-        let mut nines = vec![];
+
         for r in 0..r_size {
             for c in 0..c_size {
-                energy[r][c] += 1;
-                if energy[r][c] > 9 {
-                    nines.push((r, c))
+                match energy[r][c] {
+                    e if e == -i - 9 => {
+                        continue;
+                    }
+                    e if e < 0 => {
+                        energy[r][c] += i + 9;
+                    }
+                    _ => {
+                        energy[r][c] += 1;
+                    }
                 }
-            }
-        }
-
-        for nine in nines {
-            if energy[nine.0][nine.1] == -1 {
-                continue;
-            }
-            let mut stack = vec![nine];
-            energy[nine.0][nine.1] = -1;
-            step_flashes.push(nine);
-            while let Some((r, c)) = stack.pop() {
-                for (rr, cc) in neighbors(r_size, c_size, r, c, &energy) {
-                    energy[rr][cc] += 1;
-                    if energy[rr][cc] > 9 {
-                        stack.push((rr, cc));
-                        energy[rr][cc] = -1;
-                        step_flashes.push((rr, cc));
+                if energy[r][c] > 9 {
+                    let mut stack = vec![(r, c)];
+                    energy[r][c] = -i - 9;
+                    while let Some((r, c)) = stack.pop() {
+                        step_flashes += 1;
+                        for (rr, cc) in neighbors(r_size, c_size, r, c, &energy, i) {
+                            energy[rr][cc] += 1;
+                            if energy[rr][cc] > 9 {
+                                stack.push((rr, cc));
+                                energy[rr][cc] = -i - 9;
+                            }
+                        }
                     }
                 }
             }
         }
-        if step_flashes.len() == r_size * c_size && p2 {
+        if step_flashes == r_size * c_size && p2 {
             return i + 1;
         }
-        for &(r, c) in &step_flashes {
-            energy[r][c] = 0;
-        }
-        flashes += step_flashes.len() as i32;
+        flashes += step_flashes as i16;
     }
     unreachable!()
 }
