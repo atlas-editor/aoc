d762668ea17dd510e798bcf5273a78a5749b9f21
ae <david.uhrik@resistant.ai>
2024-07-31 16:44:48 +0200

    refactor but still slow hmm

--- 2021/aoc-2021-rust/src/days/d15.rs
+++ 2021/aoc-2021-rust/src/days/d15.rs
@@ -1,86 +1,76 @@
+use crate::days::utils::Matrix;
+use crate::matrix;
 use itertools::Itertools;
 use std::collections::BinaryHeap;
 
-pub fn p1(raw_input: &str) -> i32 {
+pub fn p1(raw_input: &[u8]) -> i32 {
     let g = parse_input(raw_input);
-    let (r_size, c_size) = (g.len(), g[0].len());
-    dijkstra(&g, (0, 0), (r_size as i32 - 1, c_size as i32 - 1))
+    dijkstra(&g)
 }
 
-pub fn p2(raw_input: &str) -> i32 {
+pub fn p2(raw_input: &[u8]) -> i32 {
     let gg = extend_graph(&parse_input(raw_input));
-    let (r_size, c_size) = (gg.len(), gg[0].len());
-    dijkstra(&gg, (0, 0), (r_size as i32 - 1, c_size as i32 - 1))
+    dijkstra(&gg)
 }
 
-fn parse_input(raw_input: &str) -> Vec<Vec<u8>> {
-    raw_input
-        .lines()
-        .map(|x| {
-            x.chars()
-                .map(|y| y.to_digit(10).unwrap() as u8)
-                .collect_vec()
-        })
-        .collect()
+fn parse_input(raw_input: &[u8]) -> Matrix<i32> {
+    Matrix::from_digits(raw_input)
 }
 
-fn wrap(x: usize) -> u8 {
+fn wrap(x: i32) -> i32 {
     if x <= 9 {
-        x as u8
+        x
     } else {
-        ((x % 10) + 1) as u8
+        (x % 10) + 1
     }
 }
 
-fn extend_graph(g: &Vec<Vec<u8>>) -> Vec<Vec<u8>> {
-    let (r_size, c_size) = (g.len(), g[0].len());
-    let mut gg = vec![vec![0; 5 * c_size]; 5 * r_size];
-
-    for i in 0..r_size {
+fn extend_graph(g: &Matrix<i32>) -> Matrix<i32> {
+    let mut gg = matrix![0; 5*g.c_size(), 5*g.r_size()];
+    for i in 0..g.r_size() {
         for j in 0..5 {
-            for k in 0..c_size {
+            for k in 0..g.c_size() {
                 for l in 0..5 {
-                    gg[i + (j * r_size)][k + (l * c_size)] = wrap(g[i][k] as usize + j + l)
+                    gg[(i + (j * g.r_size()), k + (l * g.c_size()))] =
+                        wrap(g[(i, k)] + j as i32 + l as i32)
                 }
             }
         }
     }
-
     gg
 }
 
-fn nbrs(r: usize, c: usize, u: (i32, i32)) -> Vec<(i32, i32)> {
+fn nbrs(r: usize, c: usize, u: (usize, usize)) -> impl Iterator<Item = (usize, usize)> {
     [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        .map(|x| (x.0 + u.0, x.1 + u.1))
+        .map(|x| (x.0 + (u.0 as i32), x.1 + (u.1 as i32)))
         .into_iter()
-        .filter(|&x| x.0 >= 0 && x.0 < r as i32 && x.1 >= 0 && x.1 < c as i32)
-        .collect_vec()
+        .filter(move |&x| x.0 >= 0 && x.0 < r as i32 && x.1 >= 0 && x.1 < c as i32)
+        .map(|y| (y.0 as usize, y.1 as usize))
 }
 
-fn dijkstra(g: &Vec<Vec<u8>>, start: (i32, i32), dest: (i32, i32)) -> i32 {
-    let (r_size, c_size) = (g.len(), g[0].len());
-    let mut dist = vec![vec![i32::MAX; c_size]; r_size];
-    let mut q = BinaryHeap::new();
+fn dijkstra(g: &Matrix<i32>) -> i32 {
+    let mut dist = matrix![i32::MAX; g.r_size(), g.c_size()];
+    let mut heap = BinaryHeap::new();
 
-    dist[start.0 as usize][start.1 as usize] = 0;
-    q.push((0, start));
-    while let Some((d, u)) = q.pop() {
-        if u == dest {
+    dist[(0, 0)] = 0;
+    heap.push((0, (0, 0)));
+    while let Some((d, u)) = heap.pop() {
+        if u == (g.r_size() - 1, g.c_size() - 1) {
             return -d;
         }
-        if -d > dist[u.0 as usize][u.1 as usize] {
+        if -d > dist[(u.0, u.1)] {
             continue;
         }
 
-        for v in nbrs(r_size, c_size, u) {
-            let alt = dist[u.0 as usize][u.1 as usize] + g[v.0 as usize][v.1 as usize] as i32;
-            if alt < dist[v.0 as usize][v.1 as usize] {
-                dist[v.0 as usize][v.1 as usize] = alt;
-                q.push((-alt, v));
+        for v in nbrs(g.r_size(), g.c_size(), (u.0, u.1)) {
+            let alt = dist[(u.0, u.1)] + g[(v.0, v.1)];
+            if alt < dist[(v.0, v.1)] {
+                dist[(v.0, v.1)] = alt;
+                heap.push((-alt, (v.0, v.1)));
             }
         }
     }
-    0
+    panic!("destination not reached")
 }
 
 #[cfg(test)]
@@ -93,8 +83,8 @@ mod tests {
         assert_eq!(p2(raw_input()), 315);
     }
 
-    fn raw_input<'a>() -> &'a str {
-        "1163751742
+    fn raw_input<'a>() -> &'a [u8] {
+        b"1163751742
 1381373672
 2136511328
 3694931569
--- 2021/aoc-2021-rust/src/main.rs
+++ 2021/aoc-2021-rust/src/main.rs
@@ -2,6 +2,6 @@ mod days;
 mod utils;
 
 fn main() {
-    run!(d13);
-    benchmark_all!(d13);
+    run!(d15);
+    benchmark_all!(d15);
 }
