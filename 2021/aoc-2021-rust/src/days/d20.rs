21d63ccfdad10bad60ba5bd0cbc45fc79fffa184
ae <david.uhrik@resistant.ai>
2024-09-05 20:01:58 +0200

    day20 refactor

--- 2021/aoc-2021-rust/src/days/d20.rs
+++ 2021/aoc-2021-rust/src/days/d20.rs
@@ -16,12 +16,12 @@ pub fn p2(raw_input: &[u8]) -> usize {
     input_image.lit_pixels()
 }
 
-fn parse_input(raw_input: &[u8]) -> (IEAlgorithm, InputImage) {
-    let (iealgo, inputim) = raw_input.split_once_str(b"\n\n").unwrap();
+fn parse_input(raw_input: &[u8]) -> (IEAlgorithm, Image) {
+    let (ie_algorithm, input_image) = raw_input.split_once_str(b"\n\n").unwrap();
 
     (
-        IEAlgorithm::from_input(iealgo),
-        InputImage::from_input(inputim),
+        IEAlgorithm::from_input(ie_algorithm),
+        Image::from_input(input_image),
     )
 }
 
@@ -31,61 +31,50 @@ enum Pixel {
     Dark,
 }
 
-impl Display for Pixel {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Pixel::Light => {
-                write!(f, "#")?;
-            }
-            Pixel::Dark => {
-                write!(f, ".")?;
-            }
-        }
-        Ok(())
-    }
-}
-
 #[derive(Debug, Clone, Copy)]
 struct IEAlgorithm {
-    algo: [Pixel; 512],
+    algorithm: [Pixel; 512],
 }
 
 impl IEAlgorithm {
     fn from_input(input: &[u8]) -> Self {
-        let mut algo = [Pixel::Dark; 512];
+        let mut algorithm = [Pixel::Dark; 512];
 
         for (i, &byte) in input.iter().enumerate() {
             if byte == b'#' {
-                algo[i] = Pixel::Light
+                algorithm[i] = Pixel::Light
             }
         }
 
-        Self { algo }
+        Self { algorithm }
     }
 
     fn full(&self, pixel: Pixel) -> Pixel {
         match pixel {
-            Pixel::Light => self[511],
-            Pixel::Dark => self[0],
+            Pixel::Light => self.algorithm[511],
+            Pixel::Dark => self.algorithm[0],
         }
     }
 }
 
-impl Index<usize> for IEAlgorithm {
+impl Index<[Pixel; 9]> for IEAlgorithm {
     type Output = Pixel;
 
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.algo[index]
+    fn index(&self, index: [Pixel; 9]) -> &Self::Output {
+        &self.algorithm[index.iter().fold(0usize, |acc, p| match p {
+            Pixel::Light => acc << 1 | 1,
+            Pixel::Dark => acc << 1,
+        })]
     }
 }
 
 #[derive(Debug)]
-struct InputImage {
+struct Image {
     image: Matrix<Pixel>,
     infinite_pixel: Pixel,
 }
 
-impl InputImage {
+impl Image {
     fn from_input(input: &[u8]) -> Self {
         Self {
             image: Matrix::from_repr(
@@ -96,30 +85,30 @@ impl InputImage {
         }
     }
 
-    fn _apply_ie_algorithm(&mut self, ie_algorithm: &IEAlgorithm) {
+    fn apply_ie_algorithm_once(&mut self, ie_algorithm: &IEAlgorithm) {
         let mut new_data =
             Vec::with_capacity((self.image.r_size() + 2) * (self.image.c_size() + 2));
         for i in -1..(self.image.r_size() as i32) + 1 {
             for j in -1..(self.image.c_size() as i32) + 1 {
-                let mut k = [self.infinite_pixel; 9];
-                for (idx, n) in nbrs(self.image.r_size(), self.image.c_size(), (i, j)) {
-                    k[idx] = self.image[n]
+                let mut neighborhood = [self.infinite_pixel; 9];
+                for (idx, n) in neighbors(self.image.r_size(), self.image.c_size(), (i, j)) {
+                    neighborhood[idx] = self.image[n]
                 }
-                new_data.push(ie_algorithm[nbrhood_to_num(&k)])
+                new_data.push(ie_algorithm[neighborhood])
             }
         }
-        let new_matrix = Matrix::from_shape_and_data(
+        let new_image = Matrix::from_shape_and_data(
             (self.image.r_size() + 2, self.image.c_size() + 2),
             new_data,
         );
 
-        self.image = new_matrix;
+        self.image = new_image;
         self.infinite_pixel = ie_algorithm.full(self.infinite_pixel);
     }
 
     fn apply_ie_algorithm(&mut self, ie_algorithm: &IEAlgorithm, count: usize) {
         for _ in 0..count {
-            self._apply_ie_algorithm(ie_algorithm);
+            self.apply_ie_algorithm_once(ie_algorithm);
         }
     }
 
@@ -132,7 +121,11 @@ impl InputImage {
     }
 }
 
-fn nbrs(r: usize, c: usize, index: (i32, i32)) -> impl Iterator<Item = (usize, (usize, usize))> {
+fn neighbors(
+    r: usize,
+    c: usize,
+    index: (i32, i32),
+) -> impl Iterator<Item = (usize, (usize, usize))> {
     [
         (-1, -1),
         (-1, 0),
@@ -149,13 +142,6 @@ fn nbrs(r: usize, c: usize, index: (i32, i32)) -> impl Iterator<Item = (usize, (
     .map(move |(idx, x)| (idx, (x.0 + index.0, x.1 + index.1)))
     .filter(move |(idx, x)| x.0 >= 0 && x.0 < r as i32 && x.1 >= 0 && x.1 < c as i32)
     .map(|(idx, y)| (idx, (y.0 as usize, y.1 as usize)))
-}
-
-fn nbrhood_to_num(nbrhood: &[Pixel; 9]) -> usize {
-    nbrhood.iter().fold(0usize, |acc, p| match p {
-        Pixel::Light => acc << 1 | 1,
-        Pixel::Dark => acc << 1,
-    })
 }
 
 #[cfg(test)]
