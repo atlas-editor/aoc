ead16fc4cbbdb983cb0bb218090c3e50a7dbe709
ae <david.uhrik@resistant.ai>
2024-12-02 06:29:12 +0100

    day2

--- 2024/README.md
+++ 2024/README.md
@@ -4,4 +4,5 @@ Benchmark on Apple M1 Pro.
 day       part 1    part 2
 ------------------------------
 day 01    0.098 ms  0.111 ms
+day 01    0.210 ms  0.350 ms
 ```
---
+++ 2024/d02/x.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	path := os.Args[1]
+
+	data, _ := os.ReadFile(path)
+	input := strings.TrimSpace(string(data))
+
+	fmt.Println(p1(input))
+	fmt.Println(p2(input))
+}
+
+func p1(input string) int {
+	lines := strings.Split(input, "\n")
+
+	safe := 0
+	for _, line := range lines {
+		report := ints(line)
+		if isSafeIncreasing(report) || isSafeDecreasing(report) {
+			safe++
+		}
+	}
+
+	return safe
+}
+
+func p2(input string) int {
+	lines := strings.Split(input, "\n")
+
+	safe := 0
+outer:
+	for _, line := range lines {
+		nums := ints(line)
+		for i := range len(nums) {
+			tmp := slices.Clone(nums)
+			report := append(tmp[:i], tmp[i+1:]...)
+			if isSafeIncreasing(report) || isSafeDecreasing(report) {
+				safe++
+				continue outer
+			}
+		}
+	}
+
+	return safe
+}
+
+func isSafeIncreasing(nums []int) bool {
+	for i := range len(nums) - 1 {
+		if !(nums[i+1]-nums[i] > 0 && nums[i+1]-nums[i] <= 3) {
+			return false
+		}
+	}
+	return true
+}
+
+func isSafeDecreasing(nums []int) bool {
+	slices.Reverse(nums)
+	return isSafeIncreasing(nums)
+}
+
+/*
+utils
+*/
+
+func atoi(s string) int {
+	r, _ := strconv.Atoi(s)
+	return r
+}
+
+func ints(s string) (r []int) {
+	tmp := strings.Fields(s)
+	for _, t := range tmp {
+		r = append(r, atoi(t))
+	}
+	return r
+}
