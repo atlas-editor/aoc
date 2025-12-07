e94531ef3e449ee977768d278d1c38a0bc4b8119
ae <david.uhrik@resistant.ai>
2025-12-07 06:42:43 +0100

    day7 aoc 2025 python

---
+++ 2025/d07/main.py
@@ -0,0 +1,66 @@
+import time
+from functools import cache
+
+INPUT = open("input.txt").read().strip()
+
+
+def p1():
+    m = INPUT.splitlines()
+    R, C = len(m), len(m[0])
+    S = -1
+    for i in range(C):
+        if m[0][i] == "S":
+            S = i
+            break
+
+    beampos = {}
+    beampos[0] = {S}
+
+    splt = 0
+    for i in range(1, R):
+        curr = set()
+        for p in beampos[i - 1]:
+            if m[i][p] == "^":
+                splt += 1
+                curr.add(p - 1)
+                curr.add(p + 1)
+            else:
+                curr.add(p)
+
+        beampos[i] = curr
+
+    return splt
+
+
+def p2():
+    m = INPUT.splitlines()
+    R, C = len(m), len(m[0])
+    S = -1
+    for i in range(C):
+        if m[0][i] == "S":
+            S = i
+            break
+
+    @cache
+    def f(r, c):
+        if r == R:
+            return 1
+
+        if m[r][c] == "^":
+            return f(r + 1, c - 1) + f(r + 1, c + 1)
+        else:
+            return f(r + 1, c)
+
+    return f(1, S)
+
+
+if __name__ == "__main__":
+    a = time.perf_counter()
+    print(f"part1={p1()}")
+    b = time.perf_counter()
+    print("part1", b - a, "s\n")
+
+    a = time.perf_counter()
+    print(f"part1={p2()}")
+    b = time.perf_counter()
+    print("part2", b - a, "s")
